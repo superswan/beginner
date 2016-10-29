b486507b9a62d30400441afa3e4c83439d89225d
Baheshtur <eudes.nvn@hotmail.com>
2016-10-29 10:08:30 -0300

    Cleaning up

--- palindrome.py
+++ palindrome.py
@@ -1,34 +1,27 @@
+print '*****************'
+print '**HELLO THERE!***'
+print '*****************'
+print '\n Welcome the the Python Palindrome Prober!\n'
+
 def palidrome():
-  print '*****************'
-  print '**HELLO THERE!***'
-  print '*****************'
-  print '\n Welcome the the Python Palindrome Prober!\n'
-  print """Please enter the word you would like to check
+
+    print """Please enter the word you would like to check
                               for example: racecar         """
-  
-  prompt = '>'
-  
-  word = raw_input(prompt )
-  
-  if word == word[::-1]:
-      print 'Yes your word is a palindrome!'
-  else:
-      print 'Sorry this word isn\'t a palindrome :('
-      
-  print '\nHave a nice day!'
-  
-  re_play = raw_input("Want to play agian?")
-  
-  if re_play.lower() == "yes":
-    palidrome()
-  else:
-    print "Your Loss!"
-  
-palidrome()   
 
+    word = raw_input(">> ")
 
+    if word == word[::-1]:
+        print 'Yes your word is a palindrome!'
+    else:
+        print 'Sorry this word isn\'t a palindrome :('
 
+    print '\nHave a nice day!'
 
+    re_play = raw_input("Want to play agian (y/n)?")
 
+    if re_play.lower().startswith("y"):
+        palidrome()
+    else:
+        print "Your Loss!"
 
-
+palidrome()
