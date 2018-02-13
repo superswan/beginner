8352e3a579f779a67f7775a6c4bcd3148b500ac3
Nikhil713 <31701437+Nikhil713@users.noreply.github.com>
2018-02-13 21:37:08 +0530

    Rename palindrome.py to palindrome1.py

---
+++ palindrome1.py
@@ -0,0 +1,10 @@
+#To check whether the word is a palindrome
+
+c=input("Enter the word:")
+if(c==c[::-1])
+        print("The word is a palindrome")
+else
+        print("The word is not a palindrome")
+
+
+
