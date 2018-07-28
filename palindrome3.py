4df713afbca5403b16b43070b4e6bf1b7892b572
Vinayaka Kamath <vinayakkamath2010@gmail.com>
2018-07-29 02:22:13 +0530

    Added few comments and Increased cde readability (#7)

--- palindrome3.py
+++ palindrome3.py
@@ -1,25 +1,25 @@
-print ('*****************')
-print ('**HELLO THERE!***')
-print ('*****************')
-print ('\nWelcome the the Python Palindrome Prober!\n')
+print('*****************')
+print('**HELLO THERE!***')
+print('*****************')
+print('\nWelcome the the Python Palindrome Prober!\n')
 
 def palindrome():
 
-    print ('''Please enter the word you would like to check
-                              for example: racecar''')
+    print('''Please enter the word you would like to check
+            for example: racecar''')
 
     word = input(">> ")
 
-    if word == word[::-1]:
-        print ('Yes, your word is a palindrome!')
+    if word == word[ : : -1]:
+        print("Yes, your word is a palindrome!")
     else:
-        print ("Sorry this word isn't a palindrome :(")
+        print("Sorry this word isn't a palindrome :(")
 
     re_play = input("Want to play again (y/n)? ")
 
     if re_play.lower().startswith("y"):
         palindrome()
     else:
-        print ('\nHave a nice day!')
+        print('\nHave a nice day!')
 
 palindrome()
