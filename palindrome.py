4f5228106db12ebe937038b23551705275f3b1d8
Ashish D'Souza <ashish.dsouza@students.polytech.k12.de.us>
2018-10-02 11:25:23 -0400

    Update palindrome.py for Python 3
    Enabled compatibility for Python 3.

--- palindrome.py
+++ palindrome.py
@@ -1,30 +1,29 @@
 """ Simple Program to Check Palindrome String"""
 
-print '*****************'
-print '**HELLO THERE!***'
-print '*****************'
-print '\n Welcome the the Python Palindrome Prober!\n'
+print('*' * 16)
+print('**HELLO THERE!**')
+print('*' * 16)
+print('\n Welcome the the Python Palindrome Prober!\n')
 
 def palidrome():
 
-    print """Please enter the word you would like to check
-                              for example: racecar         """
+    print("Please enter the word you would like to check\nFor example: racecar")
 
-    word = raw_input(">> ")
+    word = input(">> ").lower()
 
     if word == word[::-1]:							#Checks if the string doesnt change when reversed				
-        print 'Yes your word is a palindrome!'
+        print('Yes, your word is a palindrome!')
     else:
-        print 'Sorry this word isn\'t a palindrome :('
+        print('Sorry, this word isn\'t a palindrome :(')
 
-    print '\nHave a nice day!'
+    print('\nHave a nice day!')
 
-    re_play = raw_input("Want to play again (y/n)?")
+    re_play = input("Want to play again (y/n)?").lower()
 
-    if re_play.lower().startswith("y"):
+    if re_play[0] == 'y':
         palidrome()
     else:
-        print "Your Loss!"
+        print("Your Loss!")
 
 palidrome()
 
