609b91aca777669133605b5c486f98d140bfc738
Draaz180110 <46063877+Draaz180110@users.noreply.github.com>
2020-10-01 00:18:43 +0545

    Update palindrome1.py

--- palindrome1.py
+++ palindrome1.py
@@ -1,7 +1,7 @@
 #To check whether the word is a palindrome
-#i am just learning how to make pull request. Thank you!
 
-c=input("Enter the word:")
+
+c=input("Enter the word: ")
 if(c==c[::-1])
         print("The word is a palindrome")
 else
