a9787d8e7ee8b595d61c697ab54a2086b4c41fdf
Nonye Ibeanu <57794126+NonyeReeta@users.noreply.github.com>
2021-08-08 07:38:17 +0100

    used f-string for the print statement (#29)

--- bell_number.py
+++ bell_number.py
@@ -7,16 +7,16 @@
 #13  23  33  43
 #The code to print the bell triangle is as follows-
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-n=int(input("enter the number of bell"))     #taking value from the user
-bell=0                                       #initialising bell to 'zero'
-k=0                                          #initialising k to 'zero'
+n = int(input("enter the number of bell: "))     #taking value from the user
+bell = 0                                       #initialising bell to 'zero'
+k = 0                                          #initialising k to 'zero'
 for i in range(n):                         #loop for changing rows from 0 to n
     for j in range(i+1):                   #printing columns
-        if j==0 and i>0:                     #repeating the last number of previous row in new row
-            print(bell,'',end='')            #printing first number of each line
+        if j == 0 and i > 0:                     #repeating the last number of previous row in new row
+            print(bell, '', end='')            #printing first number of each line
         else:
-            k=(i**2)+1+bell                  #to generate other numbers of line
-            print(k,'',end='')               #printing other number in lines
-            bell=k                           #updating value of bell 
+            k = (i**2)+1+bell                  #to generate other numbers of line
+            print(k, '', end='')               #printing other number in lines
+            bell = k                           #updating value of bell
     print('\n')                              #for moving into next lines           
-print("last number of bell is",bell)    
+print(f"last number of bell is {bell}")
