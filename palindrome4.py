e3c6dcfb376799c839fbfe45615e9d9c734694fd
Aleena Thomas <89017393+zerru14@users.noreply.github.com>
2021-09-30 02:35:40 +0530

    "1st commit" (#32)

---
+++ palindrome4.py
@@ -0,0 +1,18 @@
+xx=input("Entre the wordplease")
+def palindrome4(x):
+    if x==x[::-1]:
+        print("Its a Palindrome!")
+    else:
+        print("Its not a Palindrome ;(")
+    print("Have a good day!!!")
+z=palindrome4(xx)
+
+
+
+
+
+
+
+
+
+
