d66d901eaa4c6de6982c890a3d8c8f5d6d842af2
Adam57055 <adamtwam85@gmail.com>
2026-03-13 11:55:33 -0400

    Enhance user interaction and readability
    Refine user greeting and clarify palindrome definition

--- palin.py
+++ palin.py
@@ -1,15 +1,16 @@
+#Introduction of Program to User
 print ('*****************')
 print ('**HELLO THERE!***')
 print ('*****************')
-print ('\nWelcome the the Python Palindrome Prober!\n')
-print ('\nPalindrome is a a word, phrase, or sequence that reads the same backwards as forwards, e.g. madam, noon or nurses run.\n')
-        
+print ('Welcome the the Python Palindrome Prober!')
+print ('Palindrome is a a word, phrase, or sequence that reads the same backwards as forwards, e.g. madam, noon or nurses run.')
+#Program understands if the word inputted is a palindrome        
 def word_is_palindrome(word):
     if word == word[::-1]:
         return True
     else:
         return False
-    
+#Inputs user for a word and answers if it is a palindrome
 def play_round():
     print ('''Please enter the word you would like to check
                               for example: racecar''')
@@ -19,15 +20,15 @@ def play_round():
         print ('Yes, your word is a palindrome!')
     else:
         print ("Sorry this word isn't a palindrome :(")
-        
+#If user wishes to continue; if not, the game will discontinue
 def keep_playing():
     re_play = input("Want to play again (y/n)? ")
     if re_play.lower().startswith("y"):
         return True
     else:
-         print ('\nHave a nice day!')
+         print ('Have a nice day!')
          return False
-        
+#Program will run as long as user indicates so
 play_round()
 while keep_playing():
     play_round()
