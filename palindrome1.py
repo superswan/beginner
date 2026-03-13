2677bd47a2d9131b7bff6a2d8ee0f010e0ae89bc
Adam57055 <adamtwam85@gmail.com>
2026-03-13 12:00:39 -0400

    Included colons to allow if and else structures to function properly; added comments for additional clarity
    Included colons to allow if and else structures to function properly; added comments for additional clarity

--- palindrome1.py
+++ palindrome1.py
@@ -1,11 +1,10 @@
 #To check whether the word is a palindrome
 
-
+#Inputs to user what word they would like to process
 c=input("Enter the word you want to check: ")
-if(c==c[::-1])
+#If word matches a palindrome's requirements, it is a palindrome
+if(c==c[::-1]):
         print("The word is a palindrome")
-else
+#If word does not match a palindrome's requirements, it is not a palindrome
+else:
         print("The word is not a palindrome")
-
-
-
